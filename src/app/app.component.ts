d694eafe3c74e124787879042141ded0e4078cac
Francesco Fornari <f.fornari@vargroup.it>
2020-07-06 10:24:36 +0200

    DELETE: Routing Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WatsonService } from './servicies/watson/watson.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'createTraining';
+  elements: any;
+  filenames: any;
+  query: string;
+
+  constructor(
+    private watson: WatsonService
+  ) { }
+
+  ngOnInit(): void {
+    this.elements = [0];
+    this.filenames = [];
+
+    // da cancellare
+    this.filenames = [
+      ["LGO-01 LGO - Linee Guida Organizzative ISO9001_2015 - Revisione 1.PDF","Anagrafica processo MIGL - MIsurazioni, analisi e Miglioramento.pdf","RS-2021 Riesame del Sistema Qualità - Anno 2019_1_12.pdf"]
+    ]
+
+  }
+
+  getFilename(query) {
+    console.log("query");
+    let tmp = [];
+    this.watson.getQueryResults(query)
+      .subscribe((data: any) => {
+        data.results.forEach(element => {
+          //console.log(element);
+          tmp.push(element.extracted_metadata.filename)
+        });
+        this.filenames.push(tmp);
+        console.log(this.filenames);
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { GenTrainingComponent } from './gen-training/gen-training.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GenTrainingComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
     HttpClientModule
   ],
   providers: [],
